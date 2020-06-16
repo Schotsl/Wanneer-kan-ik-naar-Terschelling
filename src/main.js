e073285f648d987f6959deabdfccc2387e3ba331
Schotsl <sjorsvanholst@gmail.com>
2020-06-16 23:03:24 +0200

    Switched main.js back to Vue 2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue, { createApp } from 'vue';
+import Vue from 'vue'
 import App from './App.vue'
 import vueHeadful from 'vue-headful';
 
 Vue.component('vue-headful', vueHeadful);
+Vue.config.productionTip = false
 
-createApp(App).mount('#app')
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
